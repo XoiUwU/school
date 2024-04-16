b7a79c124a2619e912abfcc601439d8866a33739
JustXoi <xmanmpls@gmail.com>
2024-04-16 14:44:26 -0500

    database project 2nd to last

--- Spring 24/QMBE 3750/Project/Matches-createdata.sql
+++ Spring 24/QMBE 3750/Project/Matches-createdata.sql
@@ -1,4 +1,17 @@
 USE OWL;
 INSERT INTO Matches (team1, team2, victory, defeat) VALUES
-(SFS, TOR, SFS, TOR)
-;
+(SFS, TOR, SFS, TOR),
+(WAS, LDN, WAS, LDN),
+(VAN, FLA, FLA, VAN),
+(NYE, ATL, ATL, NYE),
+(HOU, GLA, HOU, GLA),
+(VAN, VAL, VAN, VAL),
+(DAL, GZC, GZC, DAL),
+(HZS, SHD, HZS, SHD),
+(WAS, LVE, WAS, LVE),
+(VAL, HOU, HOU, VAL),
+(NYE, BOS, BOS, NYE),
+(SFS, ATL, ATL, SFS),
+(LVE, FLA, FLA, LVE),
+(LDN, BOS, BOS, LDN),
+(TOR, GLA, GLA, TOR);
